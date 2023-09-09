0d982738c77fd4a55bee396f118ace2e98a9f930
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-09-09 02:31:35 -0300

    create aula013 e 014

---
+++ aula013/ambiente.js
@@ -0,0 +1,20 @@
+var c = 1
+do { 
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6)
+
+/*var c = 1
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++ // c = c + 1
+}*/
+
+/*
+console.log('Tudo ok')
+console.log('Tudo ok')
+console.log('Tudo ok')
+console.log('Tudo ok')
+console.log('Tudo ok')
+*/
+
