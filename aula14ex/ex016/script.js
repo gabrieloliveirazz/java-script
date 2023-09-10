bfb31039efa04df410e1b618f6d1343e87a3ad09
GabrielOliveiraMacedoMoura <142449789+GabrielOliveiraMacedoMoura@users.noreply.github.com>
2023-09-10 01:21:31 -0300

    ex016

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,32 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = ('Impossível contar.')
+        alert('Algo de errado não está certo')      
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <=0 ){
+            alert('Passo inválido! Considerando passo 1')
+        }
+        if (i < f){
+            //Contagem Crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1f449}`
+            }
+            res.innerHTML += `\u{1f3c1}`
+        } else {
+            //Contagem Regressiva
+            for(let c =i ; c >= f ; c-= p) {
+                res.innerHTML += `${c}\u{1f449}`
+            }
+            res.innerHTML += `\u{1f3c1}`
+        } 
+    }
+}
